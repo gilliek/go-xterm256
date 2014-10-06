4f053d035efbd69f030affed176ca1e23b68c619
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-10-06 18:03:36 +0200

    remove expected output

--- xterm256/example_test.go
+++ xterm256/example_test.go
@@ -5,7 +5,7 @@
 package xterm256_test
 
 import (
-	"xterm256"
+	"github.com/gilliek/go-xterm256/xterm256"
 )
 
 func ExampleXterm256() {
@@ -18,9 +18,5 @@ func ExampleXterm256() {
 
 	// Add a background color
 	orange.SetBackground(5, 5, 5)
-
-	// Output:
-	// \033[38;5;9mFoo\033[0m
-	// \033[38;5;214mBar\033[0m
-	// \033[38;5;231m\033[38;5;255mBar\033[0m
+	xterm256.Println(orange, "Bar")
 }
